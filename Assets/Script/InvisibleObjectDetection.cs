c85a84d31c8d7892da8cc3ea1eddc2bceb21d2ef
nathanmandin-minesnancy <nathan.mandin5@etu.univ-lorraine.fr>
2020-03-17 17:36:42 +0100

    invisible object detection implementation (in progress)

---
+++ Assets/Script/InvisibleObjectDetection.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvisibleObjectDetection : MonoBehaviour
+{
+    private SpriteRenderer m_sprite;
+
+    private Color color;
+    private float alpha_chan = 0f;
+    private Vector3 objCoord;
+    private Vector3 targetCoord;
+
+    public GameObject target;
+    public float maxDistance = 2;
+    
+    
+    private void Start()
+    {
+        m_sprite = GetComponent<SpriteRenderer>();
+        color = m_sprite.color;
+        m_sprite.color = new Color(color.r, color.g, color.b, alpha_chan);
+    }
+
+    private void Update()
+    {
+        objCoord = transform.position;
+        targetCoord = target.transform.position;
+        Vector3 tmp = objCoord - targetCoord;
+        if(tmp.magnitude < maxDistance)
+        {
+            alpha_chan = 1 - tmp.magnitude / maxDistance;
+        }
+        else
+        {
+            alpha_chan = 0f;
+        }
+        Debug.Log(alpha_chan);
+        m_sprite.color = new Color(color.r, color.g, color.b, alpha_chan);
+
+    }
+
+}
