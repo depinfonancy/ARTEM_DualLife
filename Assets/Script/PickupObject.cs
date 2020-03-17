5b39025354df6556a76ea88b1fab7538866b55eb
jeanskr <jean.sikora@depinfonancy.net>
2020-03-17 16:03:00 +0100

    début de pickupObject
    début de pickupObject

---
+++ Assets/Script/PickupObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupObject : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other){
+        if (other.gameObject.CompareTag ("Player")) {
+            print("Objet ramassé!");
+            Destroy(gameObject);
+        }
+    }}
