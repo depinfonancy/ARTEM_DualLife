6465defb7cdea92bbbdc4eac6793e262e4f3011c
jeanskr <jean.sikora@depinfonancy.net>
2020-03-24 15:30:18 +0100

    fix push
    fix push

--- Assets/Script/PlayerPush.cs
+++ Assets/Script/PlayerPush.cs
@@ -1,37 +1,65 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerPush : MonoBehaviour {
+public class PlayerPush : MonoBehaviour
+{
 
-		public float distance=1f;
-		public LayerMask ObjectMask;
+ public float distance = 1f;
+ public LayerMask boxMask;
 
-		GameObject PushPullObject;
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-				Physics2D.queriesStartInColliders = false;
-				RaycastHit2D hit= Physics2D.Raycast (transform.position, Vector2.right * transform.localScale.x, distance, ObjectMask);
-	
-				if (hit.collider != null && hit.collider.gameObject.tag == "Pushable" && Input.GetKeyDown (KeyCode.E)) {
+ GameObject box;
+
+ // Update is called once per frame
+ void Update()
+ {
+
+  GameObject box = GameObject.FindWithTag("Pushable");
+
+  RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right * transform.localScale.x, distance, boxMask);
+
+  RaycastHit2D hit2 = Physics2D.Raycast(transform.position, Vector2.left * transform.localScale.x, distance, boxMask);
+
+  if (hit.collider != null && hit.collider.gameObject.tag == "Pushable" && Input.GetButtonDown("Push"))
+  {
+
+   box = hit.collider.gameObject;
+   box.GetComponent<FixedJoint2D>().connectedBody = this.GetComponent<Rigidbody2D>();
+   box.GetComponent<FixedJoint2D>().enabled = true;
+   box.GetComponent<ObjectPush>().beingPushed = true;
+   print("tu peux pousser la boîte");
+
+  }
+  else if (Input.GetButtonUp("Push"))
+  {
+   box.GetComponent<FixedJoint2D>().enabled = false;
+   box.GetComponent<ObjectPush>().beingPushed = false;
+  }
+
+  if (hit2.collider != null && hit2.collider.gameObject.tag == "Pushable" && Input.GetButtonDown("Push"))
+  {
+
+   box = hit2.collider.gameObject;
+   box.GetComponent<FixedJoint2D>().connectedBody = this.GetComponent<Rigidbody2D>();
+   box.GetComponent<FixedJoint2D>().enabled = true;
+   box.GetComponent<ObjectPush>().beingPushed = true;
+
+  }
+  else if (Input.GetButtonUp("Push"))
+  {
+   box.GetComponent<FixedJoint2D>().enabled = false;
+   box.GetComponent<ObjectPush>().beingPushed = false;
+  }
+ }
 
 
-						PushPullObject = hit.collider.gameObject;
-						PushPullObject.GetComponent<FixedJoint2D> ().connectedBody = this.GetComponent<Rigidbody2D> ();
-						PushPullObject.GetComponent<FixedJoint2D> ().enabled = true;
-						PushPullObject.GetComponent<ObjectPush> ().beingPushed = true;
+ void OnDrawGizmos()
+ {
+  Gizmos.color = Color.yellow;
 
-				} else if (Input.GetKeyUp (KeyCode.E)) {
-						PushPullObject.GetComponent<FixedJoint2D> ().enabled = false;
-						PushPullObject.GetComponent<ObjectPush> ().beingPushed = false;
-				}
-		
-		}
+  Gizmos.DrawLine(transform.position, (Vector2)transform.position + Vector2.right * transform.localScale.x * distance);
 
+  Gizmos.color = Color.red;
 
+  Gizmos.DrawLine(transform.position, (Vector2)transform.position + Vector2.left * transform.localScale.x * distance);
+ }
 }
