73e960a3df3aca303fba88ca1911d1c885d7d080
nathanmandin-minesnancy <nathan.mandin5@etu.univ-lorraine.fr>
2020-03-31 14:29:09 +0200

    Revive Fun

---
+++ Assets/Script/BackInTime.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackInTime : MonoBehaviour
+{
+
+    private Collider2D m_collider2D;
+
+    public GameObject currentHumanPlayer;
+    public GameObject currentSpiritPlayer;
+
+    private void Start()
+    {
+        m_collider2D = GetComponent<Collider2D>();
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "HumanPlayer")
+        {
+            Debug.Log("Need to be revive ...");
+            currentHumanPlayer.GetComponent<PlayerControler>().enabled = false;
+            currentSpiritPlayer.GetComponent<SpiritControler>().enabled = false;
+        }        
+    }
+
+}
