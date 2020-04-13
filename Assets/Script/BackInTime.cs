6f43ec486a6933f4ce8850544754a18a1bfc754c
nathanmandin-minesnancy <nathan.mandin5@etu.univ-lorraine.fr>
2020-04-13 20:03:26 +0200

    Revive & InvisibleObj Fun

--- Assets/Script/BackInTime.cs
+++ Assets/Script/BackInTime.cs
@@ -6,6 +6,9 @@ public class BackInTime : MonoBehaviour
 {
 
     private Collider2D m_collider2D;
+    private GameObject player;
+    private bool revive = false;
+
 
     private void Start()
     {
@@ -14,17 +17,51 @@ public class BackInTime : MonoBehaviour
 
     private void Update()
     {
-        
+        if (revive)
+        {
+            //Freeze Player
+            Rigidbody2D player_rigidbody2D = player.gameObject.GetComponent<Rigidbody2D>();
+            player_rigidbody2D.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY;
+            player.gameObject.GetComponent<PlayerControler>().enabled = false;
+
+            //Particule + Disp
+            int i = 0;
+            while (i < 100000){
+                i += 1;
+            }
+            player.GetComponent<SpriteRenderer>().enabled = false;
+
+            //Camera travelling
+            i = 0;
+            while (i < 100000)
+            {
+                i += 1;
+            }
+            player.transform.position = transform.GetChild(0).position;
+
+
+            //Particule + Appear
+            player_rigidbody2D.constraints = RigidbodyConstraints2D.None;
+            player_rigidbody2D.constraints = RigidbodyConstraints2D.FreezeRotation;
+            i = 0;
+            while (i < 100000)
+            {
+                i += 1;
+            }
+            player.GetComponent<SpriteRenderer>().enabled = true;
+            player.gameObject.GetComponent<PlayerControler>().enabled = true;
+
+            revive = false;
+        }
+
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("hey");
         if (other.gameObject.layer == 8)
         {
-            Debug.Log("Need to be revive ...");
-            other.gameObject.GetComponent<Rigidbody2D>();
-            other.gameObject.GetComponent<PlayerControler>().enabled = false;
+            revive = true;
+            player = other.gameObject;
         }
     }
 
--- Assets/Script/InvisibleObjectDetection.cs
+++ Assets/Script/InvisibleObjectDetection.cs
@@ -10,9 +10,17 @@ public class InvisibleObjectDetection : MonoBehaviour
     private float alpha_chan = 0f;
     private Vector3 objCoord;
     private Vector3 targetCoord;
+    private float time_spend_waiting = 0f;
+    private bool already_created = false;
+
 
     public GameObject target;
-    public float maxDistance = 2;
+    public GameObject parent;
+    public GameObject created_box;
+
+    public float maxDistance = 2f;
+    public float alpha_chan_lim = 0.7f;
+    public float waiting_time_lim = 3f;
     
     
     private void Start()
@@ -27,6 +35,8 @@ public class InvisibleObjectDetection : MonoBehaviour
         objCoord = transform.position;
         targetCoord = target.transform.position;
         Vector3 tmp = objCoord - targetCoord;
+
+        //Define the value of alpha channel
         if(tmp.magnitude < maxDistance)
         {
             alpha_chan = 1 - tmp.magnitude / maxDistance;
@@ -35,7 +45,25 @@ public class InvisibleObjectDetection : MonoBehaviour
         {
             alpha_chan = 0f;
         }
-        Debug.Log(alpha_chan);
+
+        //Define the time spend with an alpha channel with a certain value
+        if (alpha_chan >= alpha_chan_lim)
+        {
+            time_spend_waiting += Time.deltaTime;
+        }
+        else
+        {
+            time_spend_waiting = 0f;
+        }
+
+        //Generation of the box
+        if(time_spend_waiting >= waiting_time_lim && !already_created)
+        {
+            GameObject box = Instantiate(created_box, parent.transform);
+            box.transform.position = transform.position;
+            already_created = true;
+        }
+
         m_sprite.color = new Color(color.r, color.g, color.b, alpha_chan);
 
     }
