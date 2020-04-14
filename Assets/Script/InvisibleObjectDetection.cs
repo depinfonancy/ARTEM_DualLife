bf83a26afb89143494f0b9d0b80c595671fe8d99
nathanmandin-minesnancy <nathan.mandin5@etu.univ-lorraine.fr>
2020-04-14 18:19:23 +0200

    InvisibleObj Fun (in progress)

--- Assets/Script/InvisibleObjectDetection.cs
+++ Assets/Script/InvisibleObjectDetection.cs
@@ -7,64 +7,73 @@ public class InvisibleObjectDetection : MonoBehaviour
     private SpriteRenderer m_sprite;
 
     private Color color;
-    private float alpha_chan = 0f;
-    private Vector3 objCoord;
-    private Vector3 targetCoord;
-    private float time_spend_waiting = 0f;
-    private bool already_created = false;
+    private float alphaChan = 0f;
+    private float timeSpendWait = 0f;
+    private bool created = false;
 
 
     public GameObject target;
     public GameObject parent;
     public GameObject created_box;
 
+    public bool dectectInBothWorld = false;
+    public bool alreadyExist = false;
+
     public float maxDistance = 2f;
-    public float alpha_chan_lim = 0.7f;
-    public float waiting_time_lim = 3f;
+    public float alphaChanDetectLim = 0.7f;
+    public float TimeNeedDetect = 3f;
     
     
     private void Start()
     {
         m_sprite = GetComponent<SpriteRenderer>();
         color = m_sprite.color;
-        m_sprite.color = new Color(color.r, color.g, color.b, alpha_chan);
+        m_sprite.color = new Color(color.r, color.g, color.b, alphaChan);
     }
 
     private void Update()
     {
-        objCoord = transform.position;
-        targetCoord = target.transform.position;
-        Vector3 tmp = objCoord - targetCoord;
+        if (dectectInBothWorld)
+        {
+            alreadyExist = false;
+        }
+
+        Vector3 tmp = transform.position - target.transform.position;
 
         //Define the value of alpha channel
         if(tmp.magnitude < maxDistance)
         {
-            alpha_chan = 1 - tmp.magnitude / maxDistance;
+            alphaChan = 1 - tmp.magnitude / maxDistance;
         }
         else
         {
-            alpha_chan = 0f;
+            alphaChan = 0f;
         }
 
         //Define the time spend with an alpha channel with a certain value
-        if (alpha_chan >= alpha_chan_lim)
+        if (alphaChan >= alphaChanDetectLim)
         {
-            time_spend_waiting += Time.deltaTime;
+            timeSpendWait += Time.deltaTime;
         }
         else
         {
-            time_spend_waiting = 0f;
+            timeSpendWait = 0f;
         }
 
         //Generation of the box
-        if(time_spend_waiting >= waiting_time_lim && !already_created)
+        if(timeSpendWait > TimeNeedDetect && !created && !alreadyExist)
         {
+            if (dectectInBothWorld)
+            {
+                GameObject gm = GameObject.Find(parent.transform.parent.name + "/" + parent.name + "/" + name);
+                Destroy(gm);
+            }
             GameObject box = Instantiate(created_box, parent.transform);
             box.transform.position = transform.position;
-            already_created = true;
+            created = true;
         }
 
-        m_sprite.color = new Color(color.r, color.g, color.b, alpha_chan);
+        m_sprite.color = new Color(color.r, color.g, color.b, alphaChan);
 
     }
 
