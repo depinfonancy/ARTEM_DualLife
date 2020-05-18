0cf7b7ac54ddb0909d22205c644567421c6a56f8
nathanmandin-minesnancy <nathan.mandin5@etu.univ-lorraine.fr>
2020-05-19 00:40:02 +0200

    Game Mechanics & Prefabs
    - Change Player Controler Architecture
    - Fix Push
    - Pick Up (Button Press) to Inventory
    
    - Upgrade Player Prefabs
    - Objects Prefabs creation
    
    - Camera Boundaries Implementation

--- Assets/Script/CameraMovement.cs
+++ Assets/Script/CameraMovement.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    public float speed = 2.0f;
+    public float smoothTime = 0.3f;
     public Vector3 offset;
+    public float minZoom = 2f;
+    public float maxZoom = 2f;
+    public float meanDistance = 2f;
+
+    public float topLimit;
+    public float bottomLimit;
+    public float leftLimit;
+    public float rightLimit;
+
+    private Vector3 velocity;
 
     private GameObject NatureSetting;
     private GameObject natureHuman;
@@ -21,15 +31,49 @@ public class CameraMovement : MonoBehaviour
 
     void LateUpdate()
     {
-        float interpolation = speed * Time.deltaTime;
 
-        Vector3 pointToReach = GetCenterPoint() + offset;
+        Move();
+        //Zoom();
+        Limit();
 
-        Vector3 position = this.transform.position;
-        position.y = Mathf.Lerp(this.transform.position.y, pointToReach.y, interpolation);
-        position.x = Mathf.Lerp(this.transform.position.x, pointToReach.x, interpolation);
+    }
 
-        this.transform.position = position;
+    private void Move()
+    {
+
+        Vector3 target = GetCenterPoint() + offset;
+
+        Vector3 position = transform.position;
+        position.x = Mathf.Lerp(transform.position.x, target.x, smoothTime);
+        position.y = Mathf.Lerp(transform.position.y, target.y, smoothTime);
+
+
+        transform.position = position;
+        // transform.position = Vector3.SmoothDamp(transform.position, target, ref velocity, smoothTime);
+
+    }
+
+    private void Zoom()
+    {
+        //float interpolation = speed/4 * Time.deltaTime;
+
+        Vector3 position = transform.position;
+        float t = meanDistance / (2 * offset.z);
+        float zoom = GetGreatestDistance() / (2 * t);
+        position.z = Mathf.Lerp(transform.position.z, zoom, smoothTime);
+
+        transform.position = position;
+
+    }
+
+    private void Limit()
+    {
+        transform.position = new Vector3
+        (
+            Mathf.Clamp(transform.position.x, leftLimit, rightLimit),
+            Mathf.Clamp(transform.position.y, bottomLimit, topLimit),
+            Mathf.Clamp(transform.position.z, offset.z - minZoom, offset.z + maxZoom)
+        );
     }
 
     private Vector3 GetCenterPoint()
@@ -54,5 +98,54 @@ public class CameraMovement : MonoBehaviour
 
             return bounds.center;
         }
+    }
+
+    private float GetGreatestDistance()
+    {
+        if (NatureSetting.activeSelf)
+        {
+            natureHuman = GameObject.Find("NatureHuman");
+            industrialSpirit = GameObject.Find("IndustrialSpirit");
+
+            var bounds = new Bounds(natureHuman.transform.position, Vector3.zero);
+            bounds.Encapsulate(industrialSpirit.transform.position);
+
+            if (bounds.size.x < bounds.size.y)
+            {
+                return bounds.size.y;
+            }
+            else
+            {
+                return bounds.size.x;
+            }
+        }
+        else
+        {
+            natureSpirit = GameObject.Find("NatureSpirit");
+            industrialHuman = GameObject.Find("IndustrialHuman");
+
+            var bounds = new Bounds(industrialHuman.transform.position, Vector3.zero);
+            bounds.Encapsulate(natureSpirit.transform.position);
+
+            if (bounds.size.x < bounds.size.y)
+            {
+                return bounds.size.y;
+            }
+            else
+            {
+                return bounds.size.x;
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawLine(new Vector2(leftLimit, topLimit), new Vector2(leftLimit, bottomLimit));
+        Gizmos.DrawLine(new Vector2(rightLimit, topLimit), new Vector2(rightLimit, bottomLimit));
+        Gizmos.DrawLine(new Vector2(leftLimit, topLimit), new Vector2(rightLimit, topLimit));
+        Gizmos.DrawLine(new Vector2(leftLimit, bottomLimit), new Vector2(rightLimit, bottomLimit));
+
     }
 }
--- Assets/Script/ObjectPush.cs
+++ Assets/Script/ObjectPush.cs
@@ -3,26 +3,22 @@ using System.Collections;
 
 public class ObjectPush : MonoBehaviour {
 
-		public bool beingPushed;
-		float xPos;
+    public bool beingPushed;
+    float xPos;
 
-		public Vector3 lastPos;
-
-
-	void Start () {
-				xPos = transform.position.x;
-				lastPos = transform.position;
+	void Start ()
+    {
+		xPos = transform.position.x;
 	}
 	
 
-	void FixedUpdate () {
-
-						if (beingPushed == false) {
-								transform.position = new Vector3 (xPos, transform.position.y);
-						} else
-								xPos = transform.position.x;
-
-	
+	void FixedUpdate ()
+    {
+  
+		if (beingPushed == false) {
+				transform.position = new Vector3 (xPos, transform.position.y);
+		} else
+				xPos = transform.position.x;
 	}
 	
 }
