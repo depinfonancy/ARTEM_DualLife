c277e2d2df7ebb083cde83cd70d921e70597c343
CelianM <celian.muller-machi@depinfonancy.net>
2020-05-29 17:53:42 +0200

    Adding a button to exit from title screen
    The buttons still need a better sprite

--- Assets/Script/MenuScript.cs
+++ Assets/Script/MenuScript.cs
@@ -5,10 +5,20 @@
 /// </summary>
 public class MenuScript : MonoBehaviour
 {
+  private GUISkin skin;
+
+  void Start()
+  {
+	// Chargement de l'apparence
+    skin = Resources.Load("GUISkin") as GUISkin;
+  }
+
   void OnGUI()
   {
     const int buttonWidth = 84;
     const int buttonHeight = 60;
+
+    GUI.skin = skin;
 
     // Affiche un bouton pour démarrer la partie
     if (
@@ -20,13 +30,29 @@ public class MenuScript : MonoBehaviour
           buttonWidth,
           buttonHeight
         ),
-        "Start!"
+        "Start !"
       )
     )
     {
       // Sur le clic, on démarre le premier niveau
       // "TutoScene_AvecSprites" est le nom de la première scène que nous avons créés.
       Application.LoadLevel("TutoScene_AvecSprites");
+    }
+    if (
+      GUI.Button(
+        // Centré en x, 1/3 en y
+        new Rect(
+          Screen.width / 2 - (buttonWidth / 2),
+          (5 *Screen.height / 6) - (buttonHeight / 2),
+          buttonWidth,
+          buttonHeight
+        ),
+        "Quit !"
+      )
+    )
+    {
+      // Sur le clic, on ferme le jeu
+      Application.Quit();
     }
   }
 }
